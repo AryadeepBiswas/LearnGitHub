7a4edac37791aa6cac8bacb67ec6621c241dcc75
AryadeepBiswas <83086326+AryadeepBiswas@users.noreply.github.com>
2021-04-24 21:47:37 +0530

    Update Bubble_Sorting.java

--- Bubble_Sorting.java
+++ Bubble_Sorting.java
@@ -1,41 +1,49 @@
-import java.util.*;
+import java.util.*; //importing the package !!
 import java.io.*;
 
-public class Bubble_Sorting
+public class BubbleSort
 {
+    public void bubbleSort(int arr[]) //Creating the method so that its clear to understand the Process !
+    {   //Get the length of the Array 
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {   
+            for (int j = 0; j < n-i-1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    // Swap arr[j+1] and arr[j]
+                    int temp = arr[j]; // An temporary variable for keeping the array value !!
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }        
+        }
+    }
+       
+     /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method to test above
     public static void main(String args[])
     {
         Scanner kb = new Scanner(System.in);
-        System.out.Println("Enter the Number of Inetegers You want to enter inside the array");
+        // Calling Scanner Class For Taking Input From The User !!
         int n = kb.nextInt();
-        int ar[] = new int(n);
-        int k = 0;
-        //Taking the input for number of vslues in the array 
-        //Now we have to sort the array so that we can Get to Know how to do bubble sorting !!! 
-        System.out.println("Input the Integers")
-        for(int i = 0 ; i<=n ; i++ )
-        {
-            ar[i] = kb.nextInt();    
-        }
-        for(int i = 0 ; i<=n ; i++)
-        {
-            for(int j = 0 ; j<=n ; j++)
-            {
-                //For swapping the array by entering the value in index 1 in index 2 
-                if(ar[j]=ar[j+1])
-                {
-                    k = ar[j];
-                    ar[j] = ar[j+1];
-                    ar[j+1] = k;
-                }
+        BubbleSort ob = new BubbleSort();
+        int arr[] = new int[n];
+        ob.bubbleSort(arr);
+        //Calling the method bubbleSort for sorting the array inputed !!
+        System.out.println("Sorted array");
+        // Now Printing the Array !!
+        ob.printArray(arr);
+        //Thus again Calling the PrintArray function for printing !!
 
-            }
-        }    //For Printing the array !! 
-        
-        for(int i = 0 ; i<=n ;i++)
-        {
-            System.out.print(ar[i]);
-        }
-    }    
-    
+    }
 }
